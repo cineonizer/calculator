f8950ddab66f683cd9ac56dd76c6a0ac71c055b4
cineonizer <jamesdkiam@gmail.com>
2021-01-23 21:56:39 -0800

    too messy of a program; need to restructure/restart

--- script.js
+++ script.js
@@ -1,36 +1,89 @@
-function add(a, b) {
-    return a + b;
-}
-
-function subtract(a, b) {
-    return a - b;
-}
-
-function multiply(a, b) {
-    return a * b;
-}
-
-function divide(a, b) {
-    return a / b;
-}
-
-function operate(operator, a, b) {
-    switch (operator) {
-        case '+':
-            add(a, b);
-            break;
-        case '-':
-            subtract(a, b);
-            break;
-        case '*':
-            multiply(a, b);
-            break;
-        case '/':
-            divide(a, b);
-            break;
-    }
-}
+const displayWindow = document.querySelector('.display p1');
+let isDecimal = false;
+let operator;
+let displayValue;
+let firstValue;
+let secondValue;
 
 function getDisplay() {
-    
+    const numberButton = document.querySelectorAll('.number-button');
+    const decimalButton = document.querySelector('.decimal-button');
+    decimalButton.addEventListener('click', () => isDecimal = true);
+    numberButton.forEach(numberButton => {
+        numberButton.addEventListener('click', () => {
+            if (!isDecimal) {
+                if (displayWindow.textContent === '0.') {
+                    displayWindow.textContent = numberButton.textContent;
+                }
+                else {
+                    displayWindow.textContent = displayWindow.textContent.slice(0, -1);
+                    displayWindow.textContent += numberButton.textContent;
+                }
+                displayWindow.textContent += '.';
+            }
+            else {
+                displayWindow.textContent += numberButton.textContent;
+            }
+            displayValue = parseFloat(displayWindow.textContent);
+        });
+    });
 }
+
+function applyOperator() {
+    const addButton = document.querySelector('#add');
+    addButton.addEventListener('click', () => {
+        if (!firstValue) {
+            firstValue = displayValue;
+            reset();
+        }
+        else {
+            displayValue = parseFloat(displayWindow.textContent);
+            firstValue += displayValue;
+            displayWindow.textContent = firstValue;
+        }
+        operator = '+';
+    });
+}
+
+function applyEquals() {
+    const equalsButton = document.querySelector('.equals-button');
+    equalsButton.addEventListener('click', () => {
+        secondValue = displayValue;
+        switch (operator) {
+            case '+':
+                let sum = firstValue + secondValue;
+                if (Number.isInteger(sum)) {
+                    displayWindow.textContent = firstValue + secondValue + '.';
+                }
+                break;
+            case '-':
+                return a - b;
+                break;
+            case '*':
+                return a * b;
+                break;
+            case '/':
+                return a / b;
+                break;
+        };
+    });
+}
+
+function clearDisplay() {
+    const clearButton = document.querySelector('#clear');
+    clearButton.addEventListener('click', () => {
+        reset();
+        firstValue = 0;
+        secondValue = 0;
+    });
+}
+
+function reset() {
+    displayWindow.textContent = '0.';
+    isDecimal = false;
+}
+
+getDisplay();
+applyOperator();
+applyEquals();
+clearDisplay();
